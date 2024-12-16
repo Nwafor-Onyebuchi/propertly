6921d4d5bfc74873f42e0233c6379a9575fef15e
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2024-12-16 16:21:48 +0100

    ---

---
+++ app/actions/deleteProperty.ts
@@ -0,0 +1,54 @@
+'use server'
+
+import connectDB from "@/config/database"
+import cloudinary from "@/config/cloudinary"
+import Property from "@/models/Property"
+import {getSessionUser} from '@/utils/getSessionUser'
+import { revalidatePath } from "next/cache"
+
+async function deleteProperty (propertyId: string) {
+    await connectDB()
+    const sessionUser = await getSessionUser()
+
+    if(!sessionUser || !sessionUser.userId) {
+        throw new Error('User ID is required')
+    }
+
+    const { userId }  = sessionUser
+
+    const property = await Property.findById(propertyId)
+
+    if(!property) {
+        throw new Error('Property not found')
+    }
+
+    if(property.owner.toString() !== userId) {
+        throw new Error('Unauthorized')
+    }
+
+    const publicIds = property.images.map((imageUrl:string)=>{
+        const parts = imageUrl.split('/')
+        const imageName = parts.at(-1)
+        if(!imageName) {
+            throw new Error('images not found')
+        } 
+        return imageName.split('.').at(0)
+        
+    })
+
+    console.log(publicIds)
+
+    //  Delete images from cloudinary
+    if(publicIds.length > 0) {
+        for(const publicId of publicIds){
+            await cloudinary.uploader.destroy('propertly/' + publicId)
+        }
+    }
+
+    await property.deleteOne()
+
+    revalidatePath('/', "layout")
+
+}
+
+export default deleteProperty
--- components/ProfileProperties.tsx
+++ components/ProfileProperties.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { IProperty } from "@/types";
 import Link from "next/link";
+import deleteProperty from "@/app/actions/deleteProperty";
 
 
 const ProfilePproperties = ({ properties }) => {
@@ -10,20 +11,33 @@ const ProfilePproperties = ({ properties }) => {
 
 //   console.log('first', properties.length)
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handleDelete = async (propertyId: any) => {
+  const confirm = window.confirm('Are you sure about this?')
+
+  if(!confirm) return
+
+  await deleteProperty(propertyId)
+
+  const updatedProps = userProperties.filter((property:IProperty)=>property.id !== propertyId)
+
+  setUserProperties(updatedProps)
+}
+
   return userProperties.map((property:IProperty, index:number)=>(
     <div key={index} className="mb-10">
       <Link href={`/properties/${property.id}`}>
         <Image
           className="h-32 w-full rounded-md object-cover"
           src={property.images[0]}
-        width={200}
-        height={200}
+          width={200}
+          height={200}
           alt="Property 1"
         />
       </Link>
       <div className="mt-2">
-        <p className="text-lg font-semibold">Property Title 1</p>
-        <p className="text-gray-600">Address: 123 Main St</p>
+        <p className="text-lg font-semibold">{property.name}</p>
+        <p className="text-gray-600">Address: {`${property.location?.street}  ${property.location?.city}, ${property.location?.state}`}</p>
       </div>
       <div className="mt-2">
         <Link
@@ -34,7 +48,7 @@ const ProfilePproperties = ({ properties }) => {
         </Link>
         <button
           className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-          type="button"
+          type="button" onClick={()=>handleDelete(property.id)}
         >
           Delete
         </button>
