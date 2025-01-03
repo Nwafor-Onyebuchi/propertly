a23c590bce049b63345f4b4b170dfaccc366c8a1
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2025-01-03 14:45:55 +0100

    --

--- app/properties/save/page.tsx
+++ app/properties/save/page.tsx
@@ -23,6 +23,7 @@ const SavedPropertyPage = async () => {
         <div className="container lg:container m-auto px-4 py-6">
             <h1 className="text-2xl mb-4">Save Properties</h1>
             {bookmarks.length === 0 ? (<p>No saved properties</p>):(<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {/*  eslint-disable-next-line @typescript-eslint/no-explicit-any */}
                 {bookmarks.map((bookmark:any)=> (
                     <PropertyCard property={bookmark} key={bookmark._id}/>
                 ))}
